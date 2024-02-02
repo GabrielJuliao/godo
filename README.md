f15add67b8ce67c23c4c7c63d3d050412f39b818
gabrieljuliao <gabrielbjuliao@gmail.com>
2024-02-01 21:44:39 -0300

    Migrate config to yaml
    - Changing configuration file from JSON to yaml
    - Add support for env var conf file

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # GODO
-A simple command line tool to execute macros
+## A simple command line tool to execute macros
+
+This README provides instructions on how to build and execute a Go application on multiple platforms.
+
+## Prerequisites
+
+Make sure you have the following installed on your machine:
+
+- [Go](https://golang.org/doc/install)
+
+## Getting Started
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/GabrielJuliao/godo.git
+    
+    cd godo
+    ```
+
+2. Navigate to the directory containing the Go source code:
+
+    ```bash
+    cd cmd/godo
+    ```
+
+## Build the Go Application
+
+### Windows
+
+To build the Go application on Windows, run the following command in Command Prompt or PowerShell:
+
+```bash
+go build -o godo.exe
+```
+
+This will create an executable binary with a `.exe` extension.
+
+### macOS and Linux
+
+To build the Go application on macOS or Linux, run the following command in the terminal:
+
+```bash
+go build -o godo
+```
+
+This will create an executable binary without an extension.
+
+## Execute the Go Application
+
+### Windows
+
+After building the application, you can execute it on Windows by running:
+
+```bash
+godo.exe
+```
+
+### macOS and Linux
+
+After building the application, you can execute it on macOS or Linux by running:
+
+```bash
+./godo
+```
+
+## Clean Up
+
+If you want to clean up the generated binary, you can run:
+
+```bash
+go clean
+```
+
+This removes the compiled binary.
+
+## Additional Notes
+
+- Refer to the [official Go documentation](https://golang.org/doc/) for more details on Go programming.
--- cmd/godo/main.go
+++ cmd/godo/main.go
@@ -3,29 +3,62 @@ package main
 import (
 	"bufio"
 	b64 "encoding/base64"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Macro struct {
-	MacroName   string
-	Executable  string
-	Arguments   string
-	Description string
+	MacroName   string `yaml:"macroName"`
+	Executable  string `yaml:"executable"`
+	Arguments   string `yaml:"arguments"`
+	Description string `yaml:"description"`
 }
 
 type Configuration struct {
 	Macros []Macro
 }
 
+func verifyFilePath(path string) bool {
+	_, err := os.Stat(path)
+	if !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
 func readConfigurationFile() Configuration {
 	var config Configuration
-	filePath := getExecutablePath() + string(os.PathSeparator) + "config.json"
+	var filePath string
+	filePathEnvVar := os.Getenv("GODO_CONFIGURATION_FILE")
+	pwd := getExecutablePath() + string(os.PathSeparator)
+	filePathYaml := pwd + "config.yaml"
+	filePathYml := pwd + "config.yml"
+
+	if !verifyFilePath(filePathEnvVar) {
+		log.Println("Environment variable GODO_CONFIGURATION_FILE is not set. Falling back to default location(s)")
+		switch {
+		case verifyFilePath(filePathYaml):
+			log.Printf("Configuration file located at %s", filePathYaml)
+			filePath = filePathYaml
+		case verifyFilePath(filePathYml):
+			log.Printf("Configuration file located at %s", filePathYml)
+			filePath = filePathYml
+		default:
+			log.Fatalf("Could locate default configuration file(s) [%s, %s]", filePathYaml, filePathYml)
+		}
+	}
+
+	if verifyFilePath(filePathEnvVar) {
+		log.Printf("Configuration file located at %s", filePathEnvVar)
+		filePath = filePathEnvVar
+	}
+
 	dataBytes, err := os.ReadFile(filePath)
 
 	if err != nil {
@@ -33,12 +66,12 @@ func readConfigurationFile() Configuration {
 		log.Fatal(err)
 	}
 
-	err = json.Unmarshal(dataBytes, &config)
+	err = yaml.Unmarshal(dataBytes, &config)
 
 	if err != nil {
-		log.Println("Something went wrong when parsing the configuration file, please check if the file is a valid JSON.")
-		bStr := "ew0KICAibWFjcm9zIjogWw0KICAgIHsNCiAgICAgICJtYWNyb05hbWUiOiAiaGVsbG8tY21kIiwNCiAgICAgICJleGVjdXRhYmxlIjogImNtZC5leGUiLA0KICAgICAgImFyZ3VtZW50cyI6ICIvYyBlY2hvIEhlbGxvIFdvcmxkIiwNCiAgICAgICJkZXNjcmlwdGlvbiI6ICJFY2hvIEhlbGxvIFdvcmxkIG1lc3NhZ2UgZnJvbSBjb21tYW5kIHByb21wdCINCiAgICB9DQogIF0NCn0NCg=="
-		log.Printf("Configuration file valid format: \n%s\n", b64Decoder(bStr))
+		log.Println("Something went wrong when parsing the configuration file, please check if the file is a valid YAML.")
+		bStr := "bWFjcm9zOgotIG1hY3JvTmFtZTogaGVsbG8td29ybGQKICBleGVjdXRhYmxlOiBlY2hvCiAgYXJndW1lbnRzOiAiSGVsbG8gV29ybGQiCiAgZGVzY3JpcHRpb246IEJyb2FkY2FzdHMgJ0hlbGxvIFdvcmxkJyB0byB0aGUgaW50ZXJnYWxhY3RpYyBzaGVsbCwgbWFraW5nIGFsaWVucyBvbiBFYXJ0aCBhbmQgaW4gc3BhY2UgY3JhY2sgYSBjb3NtaWMgc21pbGUhIDop"
+		log.Printf("Valid configuration file format: \n\n%s\n\n", b64Decoder(bStr))
 		log.Fatal(err)
 	}
 
@@ -65,15 +98,17 @@ func execMacro(argv []string) {
 			for _, arg := range argv[1:] {
 				args = append(args, arg)
 			}
+			fmt.Println("")
 			fmt.Printf("Macro name: %s\n", macro.MacroName)
 			fmt.Printf("Description: %s\n", macro.Description)
 			fmt.Printf("Run: %s %s\n\n", macro.Executable, strings.Join(args, " "))
+			fmt.Println("")
 			execCmd(macro.Executable, args)
 			os.Exit(0)
 		}
 
 	}
-	fmt.Printf("%s is not a known macro.\n", argv[0])
+	log.Printf("%s is not a known macro.\n", argv[0])
 
 }
 
@@ -108,10 +143,15 @@ func b64Decoder(b64Str string) string {
 func isArgsValid(args []string) bool {
 	for _, arg := range args {
 		if arg == "godo" {
-			log.Fatal("cannot call godo inside it self.")
+			log.Fatal("Cannot call godo inside it self.")
 		}
 	}
 	return true
+}
+
+func printUsage() {
+	fmt.Println("Usage: godo [ macro name ] [ extras arguments ]")
+	fmt.Println("Note: [ extras arguments ] will be appended to the end of the string arguments, defined in the configuration file.")
 }
 
 func main() {
@@ -120,10 +160,9 @@ func main() {
 		b64Decoder("4pSM4pSA4pSQ4pSM4pSA4pSQ4pSM4pSs4pSQ4pSM4pSA4pSQDQrilIIg4pSs4pSCIOKUgiDilILilILilIIg4pSCDQrilJTilIDilJjilJTilIDilJjilIDilLTilJjilJTilIDilJggdjAuMC4xDQpodHRwczovL2dpdGh1Yi5jb20vR2FicmllbEp1bGlhby9nb2Rv"),
 	)
 	appArguments := os.Args[1:]
-	if len(appArguments) > 0 && isArgsValid(appArguments) {
+	if len(appArguments) > 0 && isArgsValid(appArguments) && appArguments[0] != "-h" && appArguments[0] != "--help" {
 		execMacro(appArguments)
 	} else {
-		fmt.Println("Usage: godo [ macro name ] [ extras arguments ]")
-		fmt.Println("Note: [ extras arguments ] will be appended to the end of the string arguments, defined in the configuration file.")
+		printUsage()
 	}
 }
