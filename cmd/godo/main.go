00ba86a6fd89bb964f50356f265ea91a56f6f2e1
gabrieljuliao <gabrielbjuliao@gmail.com>
2024-02-02 12:43:48 -0300

    Script to build release binaries from macOS

--- cmd/godo/main.go
+++ cmd/godo/main.go
@@ -18,7 +18,7 @@ func isArgsValid(args []string) bool {
 func main() {
 	fmt.Printf(
 		"\n%s\n\n",
-		b64Decoder("4pSM4pSA4pSQ4pSM4pSA4pSQ4pSM4pSs4pSQ4pSM4pSA4pSQDQrilIIg4pSs4pSCIOKUgiDilILilILilIIg4pSCDQrilJTilIDilJjilJTilIDilJjilIDilLTilJjilJTilIDilJggdjAuMC4xDQpodHRwczovL2dpdGh1Yi5jb20vR2FicmllbEp1bGlhby9nb2Rv"),
+		b64Decoder("4pSM4pSA4pSQ4pSM4pSA4pSQ4pSM4pSs4pSQ4pSM4pSA4pSQCuKUgiDilKzilIIg4pSCIOKUguKUguKUgiDilIIK4pSU4pSA4pSY4pSU4pSA4pSY4pSA4pS04pSY4pSU4pSA4pSYIHYwLjAuMi1hbHBoYQpodHRwczovL2dpdGh1Yi5jb20vR2FicmllbEp1bGlhby9nb2Rv"),
 	)
 	appArguments := os.Args[1:]
 	if len(appArguments) > 0 && isArgsValid(appArguments) && appArguments[0] != "-h" && appArguments[0] != "--help" {
