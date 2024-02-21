36ffaedfadfe20cfc71202e83791b2e207cb0148
gabrieljuliao <gabrielbjuliao@gmail.com>
2024-02-21 00:31:09 -0300

    Automate release link generation.

--- build-release-from-mac.sh
+++ build-release-from-mac.sh
@@ -1,46 +1,144 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-# Remove dist directory if it exists
-if [ -d "dist" ]; then
-  rm -r dist
-fi
+# Define log levels
+readonly LOG_LEVEL_TRACE=0
+readonly LOG_LEVEL_DEBUG=1
+readonly LOG_LEVEL_INFO=2
+readonly LOG_LEVEL_WARN=3
+readonly LOG_LEVEL_ERROR=4
 
-if [ "$1" != "clean" ]; then
+# Default log level
+LOG_LEVEL=$LOG_LEVEL_INFO
 
-VERSION=$(cat VERSION | tr -d '[:space:]')
+# Function to set log level
+set_log_level() {
+    case $1 in
+        "TRACE") LOG_LEVEL=$LOG_LEVEL_TRACE ;;
+        "DEBUG") LOG_LEVEL=$LOG_LEVEL_DEBUG ;;
+        "INFO") LOG_LEVEL=$LOG_LEVEL_INFO ;;
+        "WARN") LOG_LEVEL=$LOG_LEVEL_WARN ;;
+        "ERROR") LOG_LEVEL=$LOG_LEVEL_ERROR ;;
+        *) echo "Invalid log level: $1" >&2 ;;
+    esac
+}
 
-platforms=("linux/amd64" "linux/arm64" "windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64")
+# Function to log messages
+log() {
+    local level=$1
+    shift
+    local message="$*"
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Create output folders
-mkdir -p dist/release
+    # Check log level
+    if [ $LOG_LEVEL -le $level ]; then
+        case $level in
+            $LOG_LEVEL_TRACE) printf "\e[34m[%s] [TRACE]\e[0m %s\n" "$timestamp" "$message" ;;
+            $LOG_LEVEL_DEBUG) printf "\e[32m[%s] [DEBUG]\e[0m %s\n" "$timestamp" "$message" ;;
+            $LOG_LEVEL_INFO) printf "\e[36m[%s] [INFO]\e[0m %s\n" "$timestamp" "$message" ;;
+            $LOG_LEVEL_WARN) printf "\e[33m[%s] [WARN]\e[0m %s\n" "$timestamp" "$message" ;;
+            $LOG_LEVEL_ERROR) printf "\e[31m[%s] [ERROR]\e[0m %s\n" "$timestamp" "$message" >&2 ;;
+        esac
+    fi
+}
 
-for platform in "${platforms[@]}"; do
+buildReleaseLink() {
+    local os="$1"
+    local arch="$2"
+    local version="$3"
+    local bin_filename="$4"
+    local sha="$5"
+    local sha_filename="$6"
 
-  IFS='/' read -ra target <<< "$platform"
-  output_name="${target[0]}_${target[1]}"
+    case $os in
+        darwin)
+            os="MacOS"
+            ;;
+        linux)
+            os="Linux"
+            ;;
+        windows)
+            os="Windows"
+            ;;
+    esac
 
-  echo -e builing: "${output_name}"
+    local bin_url="[${os} ${arch}](https://github.com/GabrielJuliao/godo/releases/download/${version}/${bin_filename})"
+    local sha_url="[checksum](https://github.com/GabrielJuliao/godo/releases/download/${version}/${sha_filename})"
+    local link=" - ${bin_url} (${sha_url} / ${sha})"
+    echo "$link" >>RELEASE.md
+}
 
-    # Create ZIP for Windows
-    if [ "${target[0]}" == "windows" ]; then
-      GOOS=${target[0]} GOARCH=${target[1]} go build -o dist/"${target[0]}"/"${target[1]}"/godo.exe
-      zip -j dist/release/godo-"${VERSION}"-"$output_name".zip dist/"${target[0]}"/"${target[1]}"/godo.exe
-      cd dist/release || exit
-      shasum -a 256 godo-"${VERSION}"-"$output_name".zip | tr -d '\n' > godo-"${VERSION}"-"$output_name".sha256sum
-      cd ../../
+cleanup() {
+    log $LOG_LEVEL_INFO "Cleaning up..."
+    if [ -d "dist" ]; then
+        rm -rf dist
+        log $LOG_LEVEL_DEBUG "Removed directory 'dist'"
+    else
+        log $LOG_LEVEL_INFO "Directory 'dist' does not exist, skipping..."
     fi
 
-  # Create TAR.GZ for Linux and macOS
-  if [ "${target[0]}" == "linux" ] || [ "${target[0]}" == "darwin" ]; then
-    GOOS=${target[0]} GOARCH=${target[1]} go build -o dist/"${target[0]}"/"${target[1]}"/godo
-    tar -czf dist/release/godo-"${VERSION}"-"$output_name".tar.gz -C dist/"${target[0]}"/"${target[1]}" godo
-    cd dist/release || exit
-    shasum -a 256 godo-"${VERSION}"-"$output_name".tar.gz | tr -d '\n' > godo-"${VERSION}"-"$output_name".sha256sum
-    cd ../../
-  fi
+    if [ -f "RELEASE.md" ]; then
+        rm RELEASE.md
+        log $LOG_LEVEL_DEBUG "Removed file RELEASE.md"
+    else
+        log $LOG_LEVEL_INFO "File RELEASE.md does not exist, skipping..."
+    fi
+    log $LOG_LEVEL_INFO "Cleanup completed!"
+}
 
-done
+main() {
+    local version
+    version=$(<VERSION)
+    log $LOG_LEVEL_INFO "Starting the build process for version $version"
 
+    platforms=("linux/amd64" "linux/arm64" "windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64")
+    platforms=($(printf "%s\n" "${platforms[@]}" | sort -t'/' -k1.1))
+
+    log $LOG_LEVEL_DEBUG "Platforms to be built: ${platforms[*]}"
+
+    echo "GODO ${version} add title here" >RELEASE.md
+    mkdir -p dist/release
+
+    for platform in "${platforms[@]}"; do
+        local os
+        local arch
+        IFS='/' read -r os arch <<<"$platform"
+        local output_name="${os}_${arch}"
+
+        log $LOG_LEVEL_INFO "Building: ${output_name}"
+
+        # Build and package for Windows
+        if [ "$os" == "windows" ]; then
+            GOOS="$os" GOARCH="$arch" go build -o "dist/${os}/${arch}/godo.exe"
+            (
+                cd dist/release || exit
+                zip -j "godo-${version}-${output_name}.zip" "../${os}/${arch}/godo.exe"
+                shasum -a 256 "godo-${version}-${output_name}.zip" | awk '{print $1}' >"godo-${version}-${output_name}.sha256sum"
+            )
+            buildReleaseLink "$os" "$arch" "$version" "godo-${version}-${output_name}.zip" "$(cat dist/release/godo-${version}-${output_name}.sha256sum | awk '{print $1}')" "godo-${version}-${output_name}.sha256sum"
+        fi
+
+        # Build and package for Linux and macOS
+        if [ "$os" == "linux" ] || [ "$os" == "darwin" ]; then
+            GOOS="$os" GOARCH="$arch" go build -o "dist/${os}/${arch}/godo"
+            (
+                cd dist/release || exit
+                tar -czf "godo-${version}-${output_name}.tar.gz" -C "../${os}/${arch}" godo
+                shasum -a 256 "godo-${version}-${output_name}.tar.gz" | awk '{print $1}' >"godo-${version}-${output_name}.sha256sum"
+            )
+            buildReleaseLink "$os" "$arch" "$version" "godo-${version}-${output_name}.tar.gz" "$(cat dist/release/godo-${version}-${output_name}.sha256sum | awk '{print $1}')" "godo-${version}-${output_name}.sha256sum"
+        fi
+    done
+
+    log $LOG_LEVEL_INFO "Build process completed!"
+}
+
+# Main script execution
+set_log_level "DEBUG"
+if [[ $# -gt 0 && "$1" == "clean" ]]; then
+    cleanup
+else
+    cleanup
+    main
 fi
